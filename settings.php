fbbb55fd6a1dbd53751b128999e3f1221c51f554
Skylar Kelty <s.kelty@kent.ac.uk>
2015-06-19 10:57:34 +0100

    Add base files

---
+++ settings.php
@@ -0,0 +1,57 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Splunk log store settings.
+ *
+ * @package    logstore_splunk
+ * @copyright  2015 Skylar Kelty <S.Kelty@kent.ac.uk>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+if ($hassiteconfig) {
+    $settings->add(new admin_setting_configtext(
+        'logstore_splunk/servername',
+        get_string('servername', 'logstore_splunk'),
+        '', 'localhost', PARAM_ALPHANUMEXT
+    ));
+
+    $settings->add(new admin_setting_configtext(
+        'logstore_splunk/port',
+        get_string('port', 'logstore_splunk'),
+        '', '8089', PARAM_INT
+    ));
+
+    $settings->add(new admin_setting_configtext(
+        'logstore_splunk/username',
+        get_string('username'),
+        '', 'admin', PARAM_ALPHANUMEXT
+    ));
+
+    $settings->add(new admin_setting_configpasswordunmask(
+        'logstore_splunk/password',
+        get_string('password'),
+        '', '', PARAM_ALPHANUMEXT
+    ));
+
+    $settings->add(new admin_setting_configtext(
+        'logstore_splunk/indexname',
+        get_string('indexname', 'logstore_splunk'),
+        '', 'moodle', PARAM_ALPHANUMEXT
+    ));
+}
