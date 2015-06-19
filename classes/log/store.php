63b89bd81d5bfcf96fe848c3d2d9eed373826bdf
Skylar Kelty <s.kelty@kent.ac.uk>
2015-06-19 11:45:47 +0100

    Add log store

---
+++ classes/log/store.php
@@ -0,0 +1,72 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Splunk log store.
+ *
+ * @package    logstore_splunk
+ * @copyright  2015 Skylar Kelty <S.Kelty@kent.ac.uk>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace logstore_splunk\log;
+
+defined('MOODLE_INTERNAL') || die();
+
+class store implements \tool_log\log\writer {
+    use \tool_log\helper\store,
+        \tool_log\helper\buffered_writer;
+
+    /**
+     * Constructor.
+     */
+    public function __construct(\tool_log\log\manager $manager) {
+        $this->helper_setup($manager);
+    }
+
+    /**
+     * Should the event be ignored (== not logged)?
+     * @param \core\event\base $event
+     * @return bool
+     */
+    protected function is_event_ignored(\core\event\base $event) {
+        if ((!CLI_SCRIPT || PHPUNIT_TEST)) {
+            // Always log inside CLI scripts because we do not login there.
+            if (!isloggedin() || isguestuser()) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Finally store the events into Splunk.
+     *
+     * @param array $evententries raw event data
+     */
+    protected function insert_event_entries($evententries) {
+        // Build a valid string for Splunk.
+        $data = array_map(function($row) {
+            $row['timecreated'] = date('d/M/Y:H:i:s O', $row['timecreated']);
+            return json_encode($row);
+        }, $evententries);
+
+        foreach ($data as $row) {
+            \logstore_splunk\splunk::log($row);
+        }
+    }
+}
---
+++ classes/splunk.php
@@ -0,0 +1,110 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace logstore_splunk;
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Splunk interface.
+ */
+class splunk
+{
+    private static $instance;
+
+    private $service;
+    private $config;
+    private $buffer = array();
+
+    /**
+     * Constructor.
+     */
+    private function __construct() {
+        require_once(dirname(__FILE__) . '/../lib/splunk/Splunk.php');
+
+        $this->config = get_config('logstore_splunk');
+
+        $this->service = new \Splunk_Service(array(
+            'host' => $this->config->servername,
+            'port' => $this->config->port,
+            'username' => $this->config->username,
+            'password' => $this->config->password
+        ));
+        $this->service->login();
+
+        $this->create_index();
+    }
+
+    /**
+     * Singleton.
+     */
+    public static function instance() {
+        if (!(self::$instance instanceof self)) {
+            self::$instance = new self;
+        }
+
+        return self::$instance;
+    }
+
+    /**
+     * Destructor.
+     */
+    public function __destruct() {
+        if (!empty($this->buffer)) {
+            $this->flush();
+        }
+    }
+
+    /**
+     * Log an item with Splunk.
+     */
+    public static function log($data) {
+        $splunk = static::instance();
+        $splunk->buffer[] = $data;
+    }
+
+    /**
+     * End the buffer.
+     */
+    public function flush() {
+        if (empty($this->buffer)) {
+            return;
+        }
+
+        $stream = $this->service->getReceiver()->attach(array(
+            'host' => $this->config->servername,
+            'index' => $this->config->indexname,
+            'sourcetype' => 'moodle'
+        ));
+        \Splunk_Util::fwriteall($stream, implode("\n", $this->buffer));
+        fclose($stream);
+
+        $this->buffer = array();
+    }
+
+    /**
+     * Create our index.
+     */
+    private function create_index() {
+        $index = $this->service->getIndexes();
+
+        try {
+            $index->get($this->config->indexname);
+        } catch (\Splunk_NoSuchEntityException $e) {
+            $index->create($this->config->indexname);
+        }
+    }
+}
